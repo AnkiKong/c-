45d5416ecdf01c9b0cbe0ca8b75be54f92664154
anki <ankikong@qq.com>
2018-11-11 10:07:21 +0800

    first commit

---
+++ src/test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+#ifdef LOCAL
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    // char str[10000];
+    printf("printf");
+    printf("%d", 111111);
+    return 0;
+}
